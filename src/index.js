55c8c6aa11c9ad9ceb25c8f8dc353548552651b0
VitoZMX <Vi-69@yandex.ru>
2021-02-20 18:31:26 +0300

    feat: add code in file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(arr) {
+    if (arr === undefined || arr.length === 0 || arguments.length === 0) {
+        return 0;
+    }
+    return Math.min(...arr);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(arr) {
+    if (arr === undefined || arr.length === 0 || arguments.length === 0) {
+        return 0;
+    }
+    return Math.max(...arr);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(arr) {
+    if (arr === undefined || arr.length === 0 || arguments.length === 0) {
+        return 0;
+    }
+    let summa = 0;
+    for (let i = 0; i < arr.length; i++) {
+        summa += arr[i];
+    }
+    return summa / arr.length;
 }
